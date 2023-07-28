6a9c2fb42566d2237c67840abdab8e3dc899ac0d
rachel3486 <rachel3486@gmail.com>
2023-07-29 08:35:07 +0900

    feat: Add sort

---
+++ src/main/java/week2/rachel5004/RadixSort.java
@@ -0,0 +1,41 @@
+package week2.rachel5004;
+
+import java.util.Arrays;
+
+public class RadixSort {
+
+    public static void radixSort(int[] arr) {
+        if (arr == null || arr.length <= 1) {
+            return;
+        }
+
+        int max = Arrays.stream(arr).max().getAsInt();
+        for (int exp = 1; max / exp > 0; exp *= 10) {
+            countingSort(arr, exp);
+        }
+    }
+
+    private static void countingSort(int[] arr, int exp) {
+        int n = arr.length;
+        int[] output = new int[n];
+        int[] count = new int[10];
+
+        Arrays.fill(count, 0);
+
+        for (int i = 0; i < n; i++) {
+            count[(arr[i] / exp) % 10]++;
+        }
+
+        for (int i = 1; i < 10; i++) {
+            count[i] += count[i - 1];
+        }
+
+        for (int i = n - 1; i >= 0; i--) {
+            output[count[(arr[i] / exp) % 10] - 1] = arr[i];
+            count[(arr[i] / exp) % 10]--;
+        }
+
+        System.arraycopy(output, 0, arr, 0, n);
+    }
+
+}
